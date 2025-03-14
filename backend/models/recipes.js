d515f6abbed5e35050c1e740bb455bf8a3d8f45d
lahirurupa <lahirurupasinghe2001@gmail.com>
2025-03-14 14:13:56 +0530

    Create recipe modle

---
+++ backend/models/recipes.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const recipeSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    items_count:{
+        type: Number,
+        required: true
+    },
+    calories:{
+        type: Number,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+export default Recipe;
